8fbbe0247f861a1eef9e40ee77e3a9499dd272a2
PEDRO HENRIQUE JAOULACK DE CARVALHO <60972832+pedrojaoulack@users.noreply.github.com>
2025-06-04 22:00:40 +0000

    tentativa de criar tabela :)

--- saga-app/pondoc/source/app/app.py
+++ saga-app/pondoc/source/app/app.py
@@ -4,7 +4,7 @@ import logging
 import openpyxl
 from core.main import main
 from core.scriptsdb import create_tables
-from flask import Flask, render_template, request, abort, send_file
+from flask import Flask, render_template, request, abort, send_file, jsonify
 from flask_cors import CORS 
 
 create_tables()
@@ -13,17 +13,16 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return render_template('createReport.html', api_url=os.getenv('AppPubKey'))
+    return render_template('createReport.html') # No need for api_url here
 
-#TODO: Criar estrutura de dados
 @app.route("/createReport", methods=['POST'])
 def createReport():
     body: dict = request.get_json()
     if not body.get('beginYear') or not body.get('endYear'):
         abort(400, "Especifique uma data.")
     filename = main(body.get('beginYear'), body.get('endYear'))
-    # filename = 'core/producao.xlsx'
-    return send_file('/app/' + filename, as_attachment=True)
+    # Invés de enviar o arquivo direto, vamos retornar um nome do arquivo
+    return jsonify({"filename": filename})
 
 
 if __name__ == "__main__":
